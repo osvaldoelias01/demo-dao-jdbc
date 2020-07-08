72134f467cc687c9770f186dadd4d20e07d5978a
Osvaldo Elias <osvaldom.elias@hotmail.com>
2020-07-08 14:04:34 +0100

    SellerDaoJDBC and DaoFactory

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,5 +11,7 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        SellerDao sellerDao = DaoFactory.creatSellerDao();
     }
 }
