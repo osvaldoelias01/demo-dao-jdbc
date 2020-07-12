fd4dfc7971fe8a5a1f5f37c73f13d40fab4f04d9
Osvaldo Elias <osvaldom.elias@hotmail.com>
2020-07-12 08:26:54 +0100

    Implementando insert

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -30,6 +31,11 @@ public class Main {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("====Teste 4: seller insert===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
     }
 }
